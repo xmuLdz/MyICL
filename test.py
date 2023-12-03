69fca67231c1e0c057516d9b5e8481c2a361af9c
xmuldz <1023822090@qq.com>
2023-12-03 16:18:28 +0800

    first commit

---
+++ test.py
@@ -0,0 +1,9 @@
+# -- coding: utf-8 --
+# @Time : 2023/12/3 15:37
+# @Author : LiDingZhao
+# @Email : 1023822090@qq.com
+# @File : test.py
+# @Software: PyCharm
+print('hello word!')
+#调出VCS
+#File->Settings->Version Control->"+" ->"Project"->VCS：Subversion->点击ok
